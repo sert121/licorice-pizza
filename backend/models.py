127a40d17f4dd14c065b077705a72caa0ad76fcb
sert121 <yashrajmore29@gmail.com>
2023-03-22 15:05:29 +0530

    langchain pydantic initialized

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+
+# import basemodel from pydantic
+from pydantic import BaseModel
+
+# create pydantic models for langch
+
+class QueryVectorStore(BaseModel):
+    query:str
+    collection_name:str
+
+class CollectionName(BaseModel):
+    collection_name:str
+
+
